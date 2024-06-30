d46394838a505095bf85ff720fc32300343a5338
Mango <149910442+MangosPage@users.noreply.github.com>
2024-06-30 15:33:51 -0400

    Add files via upload

---
+++ flash/henrystickman/index.js
@@ -0,0 +1,40 @@
+var redirecttext = document.getElementById("redirct")
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting."
+},100)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting.."
+},500)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting..."
+},800)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting"
+},400)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting."
+},600)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting.."
+},300)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting..."
+},700)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting"
+},900)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting."
+},900)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting.."
+},1000)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting..."
+},1001)
+setTimeout(function(){
+  redirecttext.innerHTML = "Redirecting"
+},401)
+setTimeout(function(){
+  location.href = "/Games/flash/henrystickman/index.html"
+},2000)
