850e0206393329764e49f04d4453c872118cd10a
Mango <149910442+MangosPage@users.noreply.github.com>
2024-07-16 23:46:01 -0400

    Add files via upload

---
+++ SnakeGame/win/script.js
@@ -0,0 +1,16 @@
+function English() {
+  document.body.style.backgroundColor = "#000";
+  document.body.style.color = "#fff";
+  document.querySelector("h1").innerHTML = "Whoever moves first is gay.";
+  let audio = new Audio('export_ofoct.com.mp3');
+  audio.loop = true;
+  audio.play();
+}
+function Spanish() {
+  document.body.style.backgroundColor = "#fff";
+  document.body.style.color = "#000";
+  document.querySelector("h1").innerHTML = "Quien se mueve primero es gay.";
+  let audio = new Audio('export_ofoct.com.mp3');
+  audio.loop = true;
+  audio.play();
+}
---
+++ SnakeGame/win/youarestupid.js
@@ -0,0 +1,7 @@
+for (;;) {
+setTimeout(function(){
+  document.querySelector("h1").innerHTML = "You are an idiot!";
+  document.body.style.backgroundColor = "#fff";
+  document.body.style.color = "#000";
+},1000)
+}
